6046d52275968ad0241cb182eb957965a40dfcfc
Kha Tran <khadieptran@gmail.com>
2023-04-01 11:11:26 -0400

    Created User and Role models for spring security

---
+++ demoEcommerce/src/main/java/com/kt/rest/demoEcommerce/controllers/ProductController.java
@@ -0,0 +1,5 @@
+package com.kt.rest.demoEcommerce.controllers;
+
+
+public class ProductController {
+}
---
+++ demoEcommerce/src/main/java/com/kt/rest/demoEcommerce/models/Role.java
@@ -0,0 +1,6 @@
+package com.kt.rest.demoEcommerce.models;
+
+public enum Role {
+    USER,
+    ADMIN
+}
