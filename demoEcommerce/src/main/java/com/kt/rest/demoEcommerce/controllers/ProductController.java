b299ce650e911b5ddf7dc244c8017c59f8e10603
Kha Tran <khadieptran@gmail.com>
2023-04-02 04:11:39 -0400

    Created route to get all products and search product by name

--- demoEcommerce/src/main/java/com/kt/rest/demoEcommerce/controllers/ProductController.java
+++ demoEcommerce/src/main/java/com/kt/rest/demoEcommerce/controllers/ProductController.java
@@ -1,11 +1,30 @@
 package com.kt.rest.demoEcommerce.controllers;
 
 
+import com.kt.rest.demoEcommerce.models.shopEntities.Product;
+import com.kt.rest.demoEcommerce.repository.ProductRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    private ProductRepository productRepository;
 
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts(@RequestParam(name = "name_like", required = false, defaultValue = "") String name) {
+        if (name.isEmpty()) {
+            return productRepository.findAll();
+        } else {
+            return productRepository.findAllByNameLike(name);
+        }
+    }
 }
--- demoEcommerce/src/main/java/com/kt/rest/demoEcommerce/repository/ProductRepository.java
+++ demoEcommerce/src/main/java/com/kt/rest/demoEcommerce/repository/ProductRepository.java
@@ -3,6 +3,8 @@ package com.kt.rest.demoEcommerce.repository;
 import com.kt.rest.demoEcommerce.models.shopEntities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+import java.util.List;
 
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllByNameLike(String name);
 }
