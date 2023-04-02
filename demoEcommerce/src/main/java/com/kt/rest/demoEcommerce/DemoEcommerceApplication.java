cccd80da194ba4bc99d75cfb82f4b39d98cd318a
Kha Tran <khadieptran@gmail.com>
2023-04-02 16:51:35 -0400

    Config to allow cross origin with the React app

--- demoEcommerce/src/main/java/com/kt/rest/demoEcommerce/DemoEcommerceApplication.java
+++ demoEcommerce/src/main/java/com/kt/rest/demoEcommerce/DemoEcommerceApplication.java
@@ -2,6 +2,9 @@ package com.kt.rest.demoEcommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoEcommerceApplication {
@@ -9,5 +12,15 @@ public class DemoEcommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoEcommerceApplication.class, args);
 	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedOrigins("http://localhost:5173");
+				}
+			};
+		}
 
 }
